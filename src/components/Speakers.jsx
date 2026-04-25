c29e37b7fdb691c70e943b546d302adfa25ab515
mxzheng <34554127+mxzheng@users.noreply.github.com>
2026-04-24 22:55:01 -0400

    Update Speakers.jsx

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,12 +1,16 @@
----
----
-<section id="speaker" class="py-16 md:py-24 bg-white">
-  <div class="container mx-auto px-4">
-    <div class="max-w-4xl mx-auto text-center">
-      <h2 class="text-2xl md:text-3xl font-bold text-center mb-8 text-primary-800">
-        Schedule
-      </h2>
-      <p class="text-gray-500 text-lg italic">To be announced.</p>
-    </div>
-  </div>
-</section>
+const Speakers = () => {
+  return (
+    <section id="speakers" className="py-16 md:py-24 bg-white">
+      <div className="container mx-auto px-4">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-2xl md:text-3xl font-bold text-center mb-8 text-primary-800">
+            Keynote Speakers
+          </h2>
+          <p className="text-gray-500 text-lg italic">To be announced.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Speakers;
